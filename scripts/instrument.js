3e15e55d936e420863696c9db8edb951748063d8
Sander den Hollander <sander@webenjoyer.com>
2025-07-27 21:16:33 +0200

    Add logs

--- scripts/instrument.js
+++ scripts/instrument.js
@@ -23,6 +23,7 @@ Sentry.init({
     // Setting this option to true will send default PII data to Sentry.
     // For example, automatic IP address collection on events
     sendDefaultPii: true,
+    enableLogs: true,
 });
 
 // Profiling happens automatically after setting it up with `Sentry.init()`.
